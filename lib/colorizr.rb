9d92410433a0b7b45d2cb122815a6bc341be5a6e
José Robles <joseroblesoneto@gmail.com>
2016-05-12 19:42:09 -0300

    Initial commit

---
+++ lib/colorizr.rb
@@ -0,0 +1,13 @@
+class String
+ color_codes = { red: 31,
+ 	green: 32,
+ 	yellow: 33,
+ 	blue: 34,
+ 	pink: 95,
+ 	light_blue: 94, 
+ 	white: 97,
+ 	light_grey: 37,
+ 	black: 30}
+
+ "puts "\e[34mThis is blue text.\e[0m" 
+end
