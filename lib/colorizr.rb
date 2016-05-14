6af84b49bdba01b04ac96bc2fbf1ff6d7a82cbc4
José Robles <joseroblesoneto@gmail.com>
2016-05-14 09:55:45 -0300

    All colors working

--- lib/colorizr.rb
+++ lib/colorizr.rb
@@ -1,48 +1,35 @@
 class String
+  
   @@color_codes = {red: 31,
   	green: 32,
 	yellow: 33,
 	blue: 34,
-	pink: 95,
-	light_blue: 94, 
-	white: 97,
+	pink: 21,
+	light_blue: 22, 
+	white: 1,
 	light_grey: 37,
 	black: 30}
 
+  def self.colors
+    @@color_codes.keys
+  end
+
   def self.create_colors
   	@@color_codes.each do |key, value|
-  	  self.send(:define_method, "#{key}") do
+  	  send(:define_method, "#{key}") do
   	  	strng = "\e[#{value}m#{self}\e[0m"
   	  	return strng 
   	  end
     end
   end
 
-  def self.colors
-    @@color_codes.keys
+  def self.sample_colors
+    colors.each do |color|
+  	  puts "This is " + "#{color.to_s.split("_").map(&:capitalize).join(' ')}".send(color)
+    end
+    return nil
   end
 
-  def self.sample_colors
-  	self.colors.each do |color|
-  		puts "This is " + "#{color.to_s.split("_").map(&:capitalize).join(' ')}".send(color)
-  	end
-  end
- #puts "\e[34mThis is blue text.\e[0m" 
 end
 
-#Test code below, erase when finished
 String.create_colors
-
-p String.colors
-
-puts "this should be red".red
-puts "this should be green".green
-puts "this should be yellow".yellow
-puts "this should be blue".blue
-puts "this should be pink".pink
-puts "this should be light blue".light_blue
-puts "this should be white".white
-puts "this should be light grey".light_grey
-puts "this should be black".black
-
-String.sample_colors
