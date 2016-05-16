ef963f7e185d5de5d4abfddb101a570a3ce8aa7c
José Robles <joseroblesoneto@gmail.com>
2016-05-16 18:41:54 -0400

    Correct colors

--- lib/colorizr.rb
+++ lib/colorizr.rb
@@ -1,14 +1,14 @@
 class String
   
   @@color_codes = {red: 31,
-  	green: 32,
-	yellow: 33,
-	blue: 34,
-	pink: 21,
-	light_blue: 22, 
-	white: 1,
-	light_grey: 37,
-	black: 30}
+    green: 32,
+	  yellow: 33,
+	  blue: 34,
+	  pink: 95,
+	  light_blue: 94, 
+	  white: 97,
+	  light_grey: 37,
+    black: 30}
 
   def self.colors
     @@color_codes.keys
